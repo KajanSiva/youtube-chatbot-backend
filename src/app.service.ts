1e8de8e8865937b3e63c909deb2c1af1ea6fb145
Kajan Siva <716149+KajanSiva@users.noreply.github.com>
2024-02-07 17:52:29 +0100

    feature: create embeddings from the transcription and store them in a memory vector store

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { YoutubeLoader } from 'langchain/document_loaders/web/youtube';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
   async getHello(): Promise<string> {
+    const openAIApiKey = this.configService.get<string>('OPENAI_API_KEY');
+    console.log('openAIApiKey: ', openAIApiKey);
+
     const videoUrl = 'https://www.youtube.com/watch?v=Kxe8VxdtrGw&t=712s';
     const loader = YoutubeLoader.createFromUrl(videoUrl, {
       language: 'fr',
@@ -19,6 +27,13 @@ export class AppService {
     console.log(docs[0].pageContent.length);
     console.log(splittedDocs.length);
     console.log(splittedDocs[0].pageContent.length);
+
+    const embeddings = new OpenAIEmbeddings({ openAIApiKey });
+
+    const vectorStore = await MemoryVectorStore.fromDocuments(
+      splittedDocs,
+      embeddings,
+    );
 
     return 'Hello World!';
   }
