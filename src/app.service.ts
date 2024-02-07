cc2e5407ec761b3786ad3cc1e15b82919a3c0c46
Kajan Siva <716149+KajanSiva@users.noreply.github.com>
2024-02-07 16:57:32 +0100

    feature: fetch youtube video transcription from an url

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { YoutubeLoader } from 'langchain/document_loaders/web/youtube';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const videoUrl = 'https://www.youtube.com/watch?v=Kxe8VxdtrGw&t=712s';
+    const loader = YoutubeLoader.createFromUrl(videoUrl, {
+      language: 'fr',
+      addVideoInfo: true,
+    });
+
+    const docs = await loader.load();
+
+    console.log(docs);
+
     return 'Hello World!';
   }
 }
